7f14e216d04cdc5b745dbb54643b3e6f11a350d3
Anuj Shrestha <livanuj@gmail.com>
2021-05-15 23:36:12 +0545

    added status and configured super admin to support approval

--- app/admin/requests.rb
+++ app/admin/requests.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Request do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :age, :phone, :gender, :urgency, :request_type, :address, :no_of_requirements, :additional_info, :offer_request_id
+  permit_params :name, :age, :phone, :gender, :urgency, :request_type, :address, :no_of_requirements, :additional_info, :offer_request_id
   #
   # or
   #
@@ -14,5 +14,23 @@ ActiveAdmin.register Request do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
+  includes :helper
+
+  index do
+    selectable_column
+    column "Id", :id
+    column "Request Type", :request_type
+    column "Urgency", :urgency
+    column "Name", :name
+    column "Phone", :phone
+    column "Status", :status
+    column "Created At", :created_at
+    column :helper, sortable: 'helpers.name'
+    # column "Offer From" do |request|
+    #   next if request.offer_request.nil?
+    #   link_to request.helper.name, admin_offer_request_path(request.offer_request)
+    # end
+    actions
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,15 +2,17 @@ class Request < ApplicationRecord
   enum gender: [:male, :female, :other]
   enum request_type: { Bed: 0, Oxygen: 1, Ventilator: 2, PCR: 3, Doctor: 4 }
   enum urgency: { NORMAL: 0, MODERATE: 1, URGENT: 2 }
+  enum status: { active: 0, approved: 1 }
 
   belongs_to :offer_request, optional: true
+  has_one :helper, through: :offer_request
 
   validates :name, presence: true
   validates :phone, presence: true
   validates :address, presence: true
   validates :urgency, presence: true
 
-  default_scope { order(created_at: :desc) }
+  default_scope { order(created_at: :asc) }
 
   def as_json(options={})
     super(:only => [:id, :request_type, :urgency, :address, :name, :no_of_requirements, :created_at])
